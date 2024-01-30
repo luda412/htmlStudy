3aca22c59e5f61383ffb5998b839bd4d68d6b345
lee <81570208+luda412@users.noreply.github.com>
2024-01-30 22:48:45 +0900

    변수와 상수, 자료형, 연산자 내용 정리

---
+++ JS_01/index.js
@@ -0,0 +1,20 @@
+let num = 10;
+num = 30;
+console.log(num);
+
+let dan = 3;
+let gugu = 8;
+let string = `${dan} 곱하기 ${gugu}은 ${dan * gugu}입니다.`;
+console.log(string);
+
+let boolean1 = 10 < 20;
+let boolean2 = 10 > 20;
+console.log(boolean1);
+console.log(boolean2);
+
+let empty = null;
+console.log(empty);
+
+let num1 = 10;
+let num2 = '10';
+console.log(num1 != num2); // = 하나는 값을 비교, 자료형은 다르지만 값이 같기 때문에(아니, 값이 똑같아)라는 관점에서 false
