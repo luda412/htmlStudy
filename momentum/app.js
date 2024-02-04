a427c69b1397a9a96b8b95ca32c62b4862f3e7e9
luda412 <201810791@sangmyung.kr>
2024-02-04 21:37:05 +0900

    feature: luda Login 기능 구현

--- momentum/app.js
+++ momentum/app.js
@@ -2,7 +2,7 @@ const loginFrom = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
-const HIDDEN_CLASSNAME = "hidden";
+const HIDDEN_CLASSNAME = "hidden";   
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
