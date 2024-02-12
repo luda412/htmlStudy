00365c9bb6a22e125711e32bf1e9b2758a38f260
luda412 <201810791@sangmyung.kr>
2024-02-12 18:12:33 +0900

    feat: router And page render

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,36 +1,14 @@
-import { useEffect, useState } from 'react'
-import Movie from './Movie'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './routes/Home'
+import Detail from './routes/Detail'
 function App() {
-  const [loading, setLoading] = useState(true)
-  const [movies, setMovies] = useState([])
-  const getMovies = async () => {
-    const json = await (
-      await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
-    ).json()
-    setMovies(json.data.movies)
-    setLoading(false)
-  }
-  useEffect(() => {
-    getMovies()
-  }, [])
   return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <Movie
-              coverImg={movie.medium_cover_image}
-              title={movie.title}
-              summary={movie.summary}
-              genres={movie.genres}
-            />
-          ))}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/movie' element={<Detail />} />
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 export default App
