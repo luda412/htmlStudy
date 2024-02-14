bf9a4371a5faf9bee46d43b7737d427ecc2e1d04
luda412 <201810791@sangmyung.kr>
2024-02-14 15:46:15 +0900

    chore: learn about navigation & layout

--- learn-nextjs14/components/navigation.tsx
+++ learn-nextjs14/components/navigation.tsx
@@ -1,9 +1,12 @@
 'use client';
+
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useState } from 'react';
 
 export default function Navigation() {
   const path = usePathname();
+  const [count, setCount] = useState(0);
   return (
     <nav>
       <ul>
@@ -13,6 +16,9 @@ export default function Navigation() {
         <li>
           <Link href="/about-us">About us</Link>
           {path === '/about-us' ? '🔥' : ''}
+        </li>
+        <li>
+          <button onClick={() => setCount((c) => c + 1)}>{count}</button>
         </li>
       </ul>
     </nav>
